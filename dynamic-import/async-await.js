ef84475e2dba9ef2f9be405a568f9574e7103127
Edy Segura <ednardo.segura@mundiale.com.br>
2021-08-30 21:32:52 -0300

    docs: rephrase async/await comments

--- dynamic-import/async-await.js
+++ dynamic-import/async-await.js
@@ -1,3 +1,3 @@
-// since Node.js v14.5 and Chrome v89 we do have support to top level await
+// since Chrome v89 and Node.js v14.5 we do have support to top level await
 const { Logger } = await import('./logger.js')
 Logger.info('This came from dynamic import!')
