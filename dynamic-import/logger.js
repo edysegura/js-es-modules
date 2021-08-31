5e9e193470ece5cb79187ae796a0d9728721e864
Edy Segura <ednardo.segura@mundiale.com.br>
2021-08-30 21:47:34 -0300

    feat: logger nows support html output

--- dynamic-import/logger.js
+++ dynamic-import/logger.js
@@ -1,9 +1,34 @@
 export class Logger {
   /**
+   * This specify where the log will be output
+   * Possible values: htmlOutput, defaultOutput
+   */
+  static output = 'console'
+
+  /**
    * Log a information
    * @param {String} message
    */
   static info(message) {
-    console.info(message)
+    Logger[this.output](message)
+  }
+
+  /**
+   * Log a information on HTML
+   * @param {String} message
+   */
+  static html(message) {
+    const textarea = document.querySelector('textarea')
+    if (textarea) {
+      textarea.value += `${message}\n`
+    }
+  }
+
+  /**
+   * Log a information on console
+   * @param {String} message
+   */
+  static console(message) {
+    console.log(message)
   }
 }
