bf0e5fd2ddf5f69de141f71308d1f747f1118855
Edy Segura <ednardo.segura@mundiale.com.br>
2021-08-30 21:19:26 -0300

    refactor: using top level await

--- dynamic-import/async-await.js
+++ dynamic-import/async-await.js
@@ -1,4 +1,3 @@
-(async () => {
-  const { Logger } = await import('./logger.js')
-  Logger.info('This came from dynamic import!')
-})()
+// since Node.js v14.5 and Chrome v89 we do have support to top level await
+const { Logger } = await import('./logger.js')
+Logger.info('This came from dynamic import!')
